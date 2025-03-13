7b75eb8e808e02498450a6b9fc66b5045cadfa68
mohd kaif <mk2818356@gmail.com>
2025-03-14 01:06:49 +0530

    added card

--- components/home/how-its-work.tsx
+++ components/home/how-its-work.tsx
@@ -1,19 +1,24 @@
-import { File } from "lucide-react"
-import { ImgHTMLAttributes } from "react"
+
+import { BrainCircuit, FileOutput, FileText, MoveRight } from "lucide-react"
 
 export function HowItsWork(){
+
     return <div>
             <div className=" flex flex-col  shadow-2xl h-32 rounded-lg m-2 mt-6 light:shadow-[#232b2b]">
             <div className="w-full text-center mt-5 dark:text-gray-400 text-gray-900/50 font-extrabold lg:text-2xl">
                 -- HOW IT WORKS -- 
             </div>
-            <div className="lg:text-4xl text-xl text-center font-bold ">
+            <div className="lg:text-4xl text-xl text-center font-bold md:text-2xl ">
                 Transform any PDF into an easy-to-digest summary in three simple steps
             </div>
             
         </div>
-        <div>
-            <Card img={<File/>} title={"AI Analysis"} subHeading={"This is sub heading"}/>
+        <div className="lg:flex md:flex md:justify-center lg:justify-center pt-9 m-1">
+            <Card img={<FileText className="h-[64] w-[64] "/>} title={"AI Analysis"} subHeading={"This is sub heading for AI Analysis"}/>
+            <div className="hidden md:block lg:block"><MoveRight className="mt-20 "/></div>
+            <Card img={<BrainCircuit className="h-[64] w-[64] "/>} title={"Brain Circuit"} subHeading={"This is sub heading for Brain Circuit"}/>
+            <div className="hidden md:block lg:block"><MoveRight className="mt-20 "/></div>
+            <Card img={<FileOutput className="h-[64] w-[64] "/>} title={"File Output"} subHeading={"This is sub heading for File Output"}/>
         </div>
     </div>
     
@@ -29,9 +34,12 @@ function Card(
         title:string,
         subHeading:string
     }){
-    return <div className="h-52  lg:w-52 bg-gray-300 mr-6 ml-6 rounded-xl hover:border  hover:border-rose-500/410">
-        {img}
-        <div className="font-bold">{title}</div> 
-        <div className="dark:text-gray-400 text-gray-900/50">{subHeading}</div>
+    return <div className="h-52  lg:w-52 relative mr-6 ml-6 rounded-xl hover:border   hover:border-rose-500/410 m-3 ">
+        <div className="w-full flex items-center justify-center pt-10 ">
+            <div className="dark:bg-[#232b2b] w-18 h-18 p-1 bg-gray-300/50 rounded-lg">{img}</div>
+        </div>
+            <div className="font-bold text-center text-2xl">{title}
+        </div> 
+        <div className="dark:text-gray-400 text-center text-gray-900/50">{subHeading}</div>
     </div>
 }
--- components/home/quick-overview.tsx
+++ components/home/quick-overview.tsx
@@ -1,7 +1,9 @@
 
 export function Overview(){
 
-    return <div className="text-card-foreground relative px-1 h-[500px] sm:h-[600px] lg:h-[700px]  xl:w-[600px] overflow-hidden bg-linear-to-br from-background via-background/95 to-rose-500/5 backdrop-blur-lg dark:shadow-[#232b2b] shadow-2xl rounded-3xl border border-rose-500/410 lg:top-[50%] m-2 lg:left-[30%] ">
-        
+
+    return <div className=" "><div className="text-card-foreground  lg:relative md:relative px-1 h-[500px] sm:h-[600px] lg:h-[700px]  xl:w-[600px] overflow-hidden bg-linear-to-br from-background via-background/95 to-rose-500/5 backdrop-blur-lg dark:shadow-[#232b2b] shadow-2xl rounded-3xl border border-rose-500/410 lg:top-[50%] m-2 lg:left-[30%]  ">
+
+        </div>
     </div>
 }
